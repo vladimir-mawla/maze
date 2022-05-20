a103070dbff86d51e1cb8491e52ad20cfaf24d27
vladimir <vladimirmawla2@gmail.com>
2022-05-21 00:43:27 +0300

    Adding eventlistener on mouseover

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,9 @@
 window.onload = (event) => {
     console.log('page is fully loaded');
+    
+    document.getElementById("start").addEventListener("mouseover", mouseOver);
+    
+    function mouseOver() {
+    document.getElementById("start").style.backgroundColor = "white";
+    }
 };
