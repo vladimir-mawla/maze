d5cdb8270e3a57d8e6e3cdfc028f1e069743198e
vladimir <vladimirmawla2@gmail.com>
2022-05-21 02:25:17 +0300

    Correcting identation in you lost function

--- scripts/game.js
+++ scripts/game.js
@@ -13,46 +13,46 @@ function start(){
 
 
 function you_lost() {
-let boundary1 = document.getElementById("boundary1")
-let boundary2 = boundary1.nextElementSibling;
-let boundary3 = boundary2.nextElementSibling;
-let boundary4 = boundary3.nextElementSibling;
-let boundary5 = boundary4.nextElementSibling;
+    let boundary1 = document.getElementById("boundary1")
+    let boundary2 = boundary1.nextElementSibling;
+    let boundary3 = boundary2.nextElementSibling;
+    let boundary4 = boundary3.nextElementSibling;
+    let boundary5 = boundary4.nextElementSibling;
 
-boundary1.addEventListener("mouseover", function( event ) {
-    boundary1.classList.add("youlose");
-    boundary2.classList.add("youlose");
-    boundary3.classList.add("youlose");
-    boundary4.classList.add("youlose");
-    boundary5.classList.add("youlose");
-})
-boundary2.addEventListener("mouseover", function( event ) {
-    boundary1.classList.add("youlose");
-    boundary2.classList.add("youlose");
-    boundary3.classList.add("youlose");
-    boundary4.classList.add("youlose");
-    boundary5.classList.add("youlose");
-})
-boundary3.addEventListener("mouseover", function( event ) {
-    boundary1.classList.add("youlose");
-    boundary2.classList.add("youlose");
-    boundary3.classList.add("youlose");
-    boundary4.classList.add("youlose");
-    boundary5.classList.add("youlose");
-})
-boundary4.addEventListener("mouseover", function( event ) {
-    boundary1.classList.add("youlose");
-    boundary2.classList.add("youlose");
-    boundary3.classList.add("youlose");
-    boundary4.classList.add("youlose");
-    boundary5.classList.add("youlose");
-})
-boundary5.addEventListener("mouseover", function( event ) {
-    boundary1.classList.add("youlose");
-    boundary2.classList.add("youlose");
-    boundary3.classList.add("youlose");
-    boundary4.classList.add("youlose");
-    boundary5.classList.add("youlose");
-})
-document.getElementById("status").innerHTML = "You Lost"
+    boundary1.addEventListener("mouseover", function( event ) {
+        boundary1.classList.add("youlose");
+        boundary2.classList.add("youlose");
+        boundary3.classList.add("youlose");
+        boundary4.classList.add("youlose");
+        boundary5.classList.add("youlose");
+    })
+    boundary2.addEventListener("mouseover", function( event ) {
+        boundary1.classList.add("youlose");
+        boundary2.classList.add("youlose");
+        boundary3.classList.add("youlose");
+        boundary4.classList.add("youlose");
+        boundary5.classList.add("youlose");
+    })
+    boundary3.addEventListener("mouseover", function( event ) {
+        boundary1.classList.add("youlose");
+        boundary2.classList.add("youlose");
+        boundary3.classList.add("youlose");
+        boundary4.classList.add("youlose");
+        boundary5.classList.add("youlose");
+    })
+    boundary4.addEventListener("mouseover", function( event ) {
+        boundary1.classList.add("youlose");
+        boundary2.classList.add("youlose");
+        boundary3.classList.add("youlose");
+        boundary4.classList.add("youlose");
+        boundary5.classList.add("youlose");
+    })
+    boundary5.addEventListener("mouseover", function( event ) {
+        boundary1.classList.add("youlose");
+        boundary2.classList.add("youlose");
+        boundary3.classList.add("youlose");
+        boundary4.classList.add("youlose");
+        boundary5.classList.add("youlose");
+    })
+    document.getElementById("status").innerHTML = "You Lost"
 }
