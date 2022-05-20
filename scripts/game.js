468d1ee423294f401edcb018c144c3227a44797a
vladimir <vladimirmawla2@gmail.com>
2022-05-21 02:18:56 +0300

    Correcting identation

--- scripts/game.js
+++ scripts/game.js
@@ -3,56 +3,56 @@ window.onload = (event) => {
     start();
 };
 
-    function start(){
-        let start = document.getElementById("start")
-        start.addEventListener("mouseover", function( event ) {
-            event.target.style.backgroundColor = "white";
-            you_lost();
-        })
-        
-    }
-
-
-    function you_lost() {
-    let boundary1 = document.getElementById("boundary1")
-    let boundary2 = boundary1.nextElementSibling;
-    let boundary3 = boundary2.nextElementSibling;
-    let boundary4 = boundary3.nextElementSibling;
-    let boundary5 = boundary4.nextElementSibling;
+function start(){
+    let start = document.getElementById("start")
+    start.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = "white";
+        you_lost();
+    })
     
-    boundary1.addEventListener("mouseover", function( event ) {
-        boundary1.classList.add("youlose");
-        boundary2.classList.add("youlose");
-        boundary3.classList.add("youlose");
-        boundary4.classList.add("youlose");
-        boundary5.classList.add("youlose");
-    })
-    boundary2.addEventListener("mouseover", function( event ) {
-        boundary1.classList.add("youlose");
-        boundary2.classList.add("youlose");
-        boundary3.classList.add("youlose");
-        boundary4.classList.add("youlose");
-        boundary5.classList.add("youlose");
-    })
-    boundary3.addEventListener("mouseover", function( event ) {
-        boundary1.classList.add("youlose");
-        boundary2.classList.add("youlose");
-        boundary3.classList.add("youlose");
-        boundary4.classList.add("youlose");
-        boundary5.classList.add("youlose");
-    })
-    boundary4.addEventListener("mouseover", function( event ) {
-        boundary1.classList.add("youlose");
-        boundary2.classList.add("youlose");
-        boundary3.classList.add("youlose");
-        boundary4.classList.add("youlose");
-        boundary5.classList.add("youlose");
-    })
-    boundary5.addEventListener("mouseover", function( event ) {
-        boundary1.classList.add("youlose");
-        boundary2.classList.add("youlose");
-        boundary3.classList.add("youlose");
-        boundary4.classList.add("youlose");
-        boundary5.classList.add("youlose");
-    })
+}
+
+
+function you_lost() {
+let boundary1 = document.getElementById("boundary1")
+let boundary2 = boundary1.nextElementSibling;
+let boundary3 = boundary2.nextElementSibling;
+let boundary4 = boundary3.nextElementSibling;
+let boundary5 = boundary4.nextElementSibling;
+
+boundary1.addEventListener("mouseover", function( event ) {
+    boundary1.classList.add("youlose");
+    boundary2.classList.add("youlose");
+    boundary3.classList.add("youlose");
+    boundary4.classList.add("youlose");
+    boundary5.classList.add("youlose");
+})
+boundary2.addEventListener("mouseover", function( event ) {
+    boundary1.classList.add("youlose");
+    boundary2.classList.add("youlose");
+    boundary3.classList.add("youlose");
+    boundary4.classList.add("youlose");
+    boundary5.classList.add("youlose");
+})
+boundary3.addEventListener("mouseover", function( event ) {
+    boundary1.classList.add("youlose");
+    boundary2.classList.add("youlose");
+    boundary3.classList.add("youlose");
+    boundary4.classList.add("youlose");
+    boundary5.classList.add("youlose");
+})
+boundary4.addEventListener("mouseover", function( event ) {
+    boundary1.classList.add("youlose");
+    boundary2.classList.add("youlose");
+    boundary3.classList.add("youlose");
+    boundary4.classList.add("youlose");
+    boundary5.classList.add("youlose");
+})
+boundary5.addEventListener("mouseover", function( event ) {
+    boundary1.classList.add("youlose");
+    boundary2.classList.add("youlose");
+    boundary3.classList.add("youlose");
+    boundary4.classList.add("youlose");
+    boundary5.classList.add("youlose");
+})
 }
