bb91a504c98ab0e0577f2e6a07eaef1009f10c5d
vladimir <vladimirmawla2@gmail.com>
2022-05-21 16:25:41 +0300

    Correcting indentation

--- scripts/game.js
+++ scripts/game.js
@@ -6,35 +6,35 @@ var score = 0;
 
 function start() {
     let start = document.getElementById("start")
-    start.addEventListener("mouseover", function( event ) {
+    start.addEventListener("mouseover", function (event) {
         bounds()
         end();
     })
-    
+
 }
 function bounds() {
     document.querySelectorAll(".boundary:not(.example)").forEach(item => {
         item.addEventListener("mouseover", you_lost)
     });
-    
+
 }
-function you_lost(){
-    if (game == true){
+function you_lost() {
+    if (game == true) {
         document.querySelectorAll(".boundary:not(.example)").forEach(item => {
             item.classList.add("youlose")
             game = false;
         });
-    
-    if (game==false){
-        score -= 10;
-        console.log(score)
-    }
-    restart()
+
+        if (game == false) {
+            score -= 10;
+            console.log(score)
+        }
+        restart()
     }
 }
 function restart() {
     if (game == false) {
-        
+
         reset_bounderies()
         reset_game()
     }
@@ -44,11 +44,11 @@ if (game == false) {
     score -= 10;
     console.log(score)
 }
-function reset_bounderies(){
-    
+function reset_bounderies() {
+
 
     let start = document.getElementById("start")
-    start.addEventListener("mouseover", function( event ) {
+    start.addEventListener("mouseover", function (event) {
         game = true;
         document.querySelectorAll(".boundary:not(.example)").forEach(item => {
             item.classList.remove("youlose")
@@ -56,27 +56,27 @@ function reset_bounderies(){
         })
     })
     start();
-    
+
 }
 function end() {
     if (game == true) {
 
         let end = document.getElementById("end")
-        end.addEventListener("mouseover", function( event ) {
+        end.addEventListener("mouseover", function (event) {
             if (game == true) {
                 score += 5;
                 console.log(score)
-            document.getElementById("status").innerHTML = "You Won";
-            game = false;
+                document.getElementById("status").innerHTML = "You Won";
+                game = false;
             }
         });
-        
+
+    }
 }
-}
-function reset_game(){
+function reset_game() {
 
     let start = document.getElementById("start")
-    start.addEventListener("click", function( event ) {
+    start.addEventListener("click", function (event) {
         game = true;
         score = 0;
         document.querySelectorAll(".boundary:not(.example)").forEach(item => {
@@ -85,5 +85,5 @@ function reset_game(){
         })
     })
     start();
-    
+
 }
