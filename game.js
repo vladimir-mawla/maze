8ac1d3d57feac0a4e573b2949deac19dc32c6c24
vladimir <vladimirmawla2@gmail.com>
2022-05-24 03:04:09 +0300

    Pulling old js file

--- game.js
+++ game.js
@@ -5,11 +5,12 @@ window.onload = (event) => {
 /* Initializing game and score variables */
 var game = true;
 var score = 0;
+var stoptime = true;
 /* Creating function to reset game */
 function reset() {
     const button = document.getElementById("start")
     button.addEventListener('click', reset_game)
-    button.addEventListener('mouseover', reset_bounderies)
+    button.addEventListener('mouseover', reset_boundaries)
 }
 /* Creating function to alert cheater */
 function alerts(){
@@ -21,6 +22,7 @@ function alerts(){
 function start() {
     let start = document.getElementById("start")
     start.addEventListener("mouseover", function (event) {
+        
         bounds()
         end();
         print_score()
@@ -30,14 +32,21 @@ function start() {
     start.addEventListener("mouseleave", function (event) { 
         startTimer()
     })
+    const best = document.getElementById('best_count');
+    best.innerHTML = "00:" + Math.min(...record);
+    const last = document.getElementById('last_count');
+    last.innerHTML = "00:" + record[-1];
+
 
 }
 /* Creating function to select all bounderies except the last one*/
 function bounds() {
     document.querySelectorAll(".boundary:not(.example)").forEach(item => {
         item.addEventListener("mouseover", you_lost)
+        
     });
-
+    
+    
 }
 /* Creating a function that ends the game */
 function you_lost() {
@@ -45,6 +54,7 @@ function you_lost() {
         document.querySelectorAll(".boundary:not(.example)").forEach(item => {
             item.classList.add("youlose")
             game = false;
+            stopTimer();
         });
 
         if (game == false) {
@@ -63,7 +73,7 @@ function restart() {
 }
 
 /* Adding function to reset boundaries */
-function reset_bounderies() {
+function reset_boundaries() {
     game = true;
     document.querySelectorAll(".boundary:not(.example)").forEach(item => {
         item.classList.remove("youlose")
@@ -81,10 +91,13 @@ function end() {
             if (game == true) {
                 score += 5;
                 print_score()
+                stopTimer();
+                resetTimer()
                 document.getElementById("status").innerHTML = "You Won";
                 game = false;
             }
         });
+        
 
     }
     reset();
@@ -93,6 +106,7 @@ function end() {
 function reset_game() {
     game = true;
     score = 0;
+    resetTimer();
     print_score();
     start();
 }
@@ -106,48 +120,66 @@ function cheater() {
         alert("Cheater!");
         score = 0;
         print_score();
+        resetTimer()
 
 }}
 
-var ms = 0;
+
+
+var min = 0;
 var sec = 0;
-var timer = false;
-let stopwatch = document.getElementById("live_count")
+record = []
 
-/*Adding start timer function */
-function startTimer () {
-    console.log("hey there!")
-    if (timer == false) {
-        timer = true;
 
-        ms = parseInt(ms);
-        sec = parseInt(sec);
-    
-        ms = ms + 1;
-    
-        if (ms == 10) {
-          sec = sec + 1;
-          ms = 0;
-        }
-        if (ms < 10) {
-        ms = '0' + ms;
-        }
-        if (sec < 10) {
-        sec = '0' + sec;
-        }
-      
-}
-}
-/* Adding stop timer function */
-function stopTimer() {
-    if (timer == true) {
-      timer = false;
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
     }
-}  
-/* Adding reset timer function */
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+    let start = document.getElementById("start")
+    start.removeEventListener("mouseover", startTimer)
+
+  }
+}
 function resetTimer() {
-    stopwatch.innerHTML = "00:00:00";
-    timer = false;
-    ms = 0;
+    const time = document.getElementById('live_count');
+    time.innerHTML = "00:00";
+    stoptime = true;
+  
     sec = 0;
+    min = 0;
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    
+    const time = document.getElementById('live_count');
+    const last = document.getElementById('last_count');
+    
+    time.innerHTML = min + ':' + sec;
+    last.innerHTML = min + ':' + sec;
+    setTimeout("timerCycle()", 1000);
+  }
 }
